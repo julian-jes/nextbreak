5be48654bf207c19491efe1717b6ccc2d502666a
Julian <julianjesacher@outlook.de>
2026-02-25 18:18:41 +0100

    Added @SerializedName to AppUpdateCheck.kt

--- app/src/main/java/com/julianjesacher/nextbreak/models/AppUpdateCheck.kt
+++ app/src/main/java/com/julianjesacher/nextbreak/models/AppUpdateCheck.kt
@@ -1,7 +1,14 @@
 package com.julianjesacher.nextbreak.models
 
+import com.google.gson.annotations.SerializedName
+
 data class AppUpdateCheck(
+    @SerializedName("last_check")
     var lastCheck: Long,
+
+    @SerializedName("last_checked_version")
     var lastCheckedVersion: List<Int>,
+
+    @SerializedName("last_release_url")
     var lastReleaseUrl: String
 )
